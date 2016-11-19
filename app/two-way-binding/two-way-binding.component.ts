cfd50d8900d7fbd16517facc396a2f405d24aa44
Louis Dasouki <louisdasou@gmail.com>
2016-11-19 15:31:39 +0000

    clean up

--- app/two-way-binding/two-way-binding.component.ts
+++ app/two-way-binding/two-way-binding.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
     selector: 'two-way-binding',
     templateUrl: 'two-way-binding.component.html'
-    // template: '<input type="text" [(ngModel])="person.name" />'
+
     
 })
 export class TwoWayBindingComponent {
